b2f96962d40e969b9419d21fb22dbe3466378401
pawel <7proPS7@gmail.com>
2020-10-18 13:09:24 +0200

    Introduction of Open API 3.0 version to the project

--- src/main/java/com/carsharing/ps/pscarsharing/PsCarSharingApplication.java
+++ src/main/java/com/carsharing/ps/pscarsharing/PsCarSharingApplication.java
@@ -1,13 +1,14 @@
 package com.carsharing.ps.pscarsharing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Car sharing API", version = "1.0.0", description = "Simple Car sharing app"))
 public class PsCarSharingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PsCarSharingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PsCarSharingApplication.class, args);
+    }
 }
