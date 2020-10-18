2f13fe4965d3db2a3517c5a3e25003ff3c6271f1
pawel <7proPS7@gmail.com>
2020-10-18 11:59:50 +0200

    Initial project for ps-car-sharing

---
+++ src/main/java/com/carsharing/ps/pscarsharing/PsCarSharingApplication.java
@@ -0,0 +1,13 @@
+package com.carsharing.ps.pscarsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsCarSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsCarSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carsharing/ps/pscarsharing/PsCarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsharing.ps.pscarsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsCarSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
